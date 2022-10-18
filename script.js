153a45a05667a0945cbf71040b5a4a667a3396ab
ryanparketh <ryanpark@rocketmail.com>
2022-10-18 11:47:29 -0400

    JS: added comments

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-// need to fetch from api given
+// create const
 const submitBtnEl = document.getElementById('search');
 const inputCityEl = document.getElementById('input-city');
-var apiKey = 'cef56e7929d1f22c4aa06d4b3d2474b1';
+const fiveDayEl = document.getElementById('five-day');
+const apiKey = 'cef56e7929d1f22c4aa06d4b3d2474b1';
 var lat, lon;
 
-console.log('in script.js');
+// added event listener when clicking search button
 submitBtnEl.addEventListener('submit', weatherSearchHandler);
 
-function weatherSearchHandler(event){
+function weatherSearchHandler(event) {
     event.preventDefault();
     var city = inputCityEl.value.trim();
     console.log('city= ', city);
     fetchGeoCoordinates(city);
 }
 
-function addToHistory(city){
+function addToHistory(city) {
     // tbd
 }
 
-function displayWeather (city, jsonData){
-    console.log ('display weather');
+// displaying the date
+function displayWeather(city, jsonData) {
     dateEl = document.getElementById('date');
     dateEl.innerText = jsonData.list[0].dt_txt;
 }
-function fetchWeather(city, lat, lon){
-    console.log ('in');
+
+// fetching the weather in the city
+function fetchWeather(city, lat, lon) {
     var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
     fetch(weatherUrl)
-        .then(function (resp){
+        .then(function (resp) {
             return resp.json();
         })
-        .then(function (jsonData){
-            
-            console.log('jsonData = ', jsonData);
-            
+        .then(function (jsonData) {
 
-            displayWeather (city, jsonData)
+            console.log('jsonData = ', jsonData);
+
+
+            displayWeather(city, jsonData)
         })
 }
-
-function fetchGeoCoordinates(city){
-    console.log ('in');
+// fetching city coordinates for lat and lon
+function fetchGeoCoordinates(city) {
     var geoCodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
     fetch(geoCodingUrl)
-        .then(function (resp){
+        .then(function (resp) {
             return resp.json();
         })
-        .then(function (jsonData){
-            if(!jsonData[0]){
+        .then(function (jsonData) {
+            if (!jsonData[0]) {
                 console.log('location not found');
-            } else{
+            } else {
                 console.log('jsonData = ', jsonData);
                 lat = jsonData[0].lat;
                 lon = jsonData[0].lon;
