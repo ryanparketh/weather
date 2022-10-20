4086dd1a9ea5e14207b774746ed662056e01170b
ryanparketh <ryanpark@rocketmail.com>
2022-10-20 12:43:32 -0400

    JS: added icon and data from json

--- script.js
+++ script.js
@@ -19,15 +19,26 @@ function addToHistory(city) {
     // tbd
 }
 
-// displaying the date
+// displaying the data
 function displayWeather(city, jsonData) {
     dateEl = document.getElementById('date');
     dateEl.innerText = jsonData.list[0].dt_txt.slice(0,10);
+    var tempEl = document.getElementById('current-temp');
+    tempEl.innerHTML = `${jsonData.list[0].main.temp}   &#176C`;
+    var humidityEl = document.getElementById('humidity');
+    humidityEl.innerText = jsonData.list[0].main.humidity + ' %';
+    var windEl = document.getElementById('wind');
+    windEl.innerText = jsonData.list[0].wind.speed + ' KMPH';
+    var iconEl = document.getElementById('current-icon');
+    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        jsonData.list[0].weather[0]["icon"]
+      }.svg`;
+    iconEl.setAttribute('src', icon);
 }
 
 // fetching the weather in the city
 function fetchWeather(city, lat, lon) {
-    var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     fetch(weatherUrl)
         .then(function (resp) {
             return resp.json();
